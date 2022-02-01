1585327c322166e46b3f39e3e79619ffaf5318a2
Frederik <fred084i@edu.aarhustech.dk>
2022-02-01 11:34:40 +0100

    geipka

--- Frederik'.py
+++ Frederik'.py
@@ -2,4 +2,4 @@ print("willian is jhhjhhjhjhjhstupid")
 print("you uhiis also nub")
 
 print("cumcumc234ucfwa2342fam")
-print("gdhhhdhhjwe4324323242342ffefeefererfrefewfwfegsddsggfawfagrgewfsdfefhuuhuh")
+print("gdhhhdhhjwe4324323242342ffefeefeefewfwefrerfrefewfwfegsddsggfawfagrgewfsdfefhuuhuh")
