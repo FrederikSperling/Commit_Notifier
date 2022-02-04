8ed13523b33c8ce9d5de2d1b974d1b8724b99fb9
Frederik <fred084i@edu.aarhustech.dk>
2022-02-04 10:45:31 +0100

    geipka 2

--- Frederik'.py
+++ Frederik'.py
@@ -2,4 +2,4 @@ print("willian is jhhafwffejhhjhjhjhstupid")
 print("you uhiis also nub")
 
 print("cumcumc234ucfwa2342fam")
-print("gdhhhd1whuuuhuhewefwefyguyg23123hhjwe43243232awdawdawd42342ffefeefeefewfwefrerfrefewfwfegsddsggfawfagrgewfsdfefhuuhuh")
+print("gdhhibhd1whuuuhuhewefwefyguyg23123hhjwe43243232awdawdawd42342ffefeefeefewfwefrerfrefewfwfegsddsggfawfagrgewfsdfefhuuhuh")
