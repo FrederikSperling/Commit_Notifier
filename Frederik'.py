249efee9fef3d267d99e7a62c6a1056c5b6d081f
Frederik <fred084i@edu.aarhustech.dk>
2022-02-04 10:18:47 +0100

    geipkaawawefewefwefgweg

--- Frederik'.py
+++ Frederik'.py
@@ -2,4 +2,4 @@ print("willian is jhhafwffejhhjhjhjhstupid")
 print("you uhiis also nub")
 
 print("cumcumc234ucfwa2342fam")
-print("gdhhhd123123hhjwe43243232awdawdawd42342ffefeefeefewfwefrerfrefewfwfegsddsggfawfagrgewfsdfefhuuhuh")
+print("gdhhhd1wewefwef23123hhjwe43243232awdawdawd42342ffefeefeefewfwefrerfrefewfwfegsddsggfawfagrgewfsdfefhuuhuh")
